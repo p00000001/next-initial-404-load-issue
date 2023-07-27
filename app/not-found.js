902e04b46a2512556eb4eddf9641909d3b727880
P <p.deason@outlook.com>
2023-07-27 11:34:52 +0100

    404 example

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    console.log("not-found.js called");
+    return (
+        <div>
+            <h2>Not Found</h2>
+            <p>Could not find requested resource</p>
+        </div>
+    )
+}
